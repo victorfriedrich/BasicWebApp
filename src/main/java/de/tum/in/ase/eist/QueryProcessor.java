cfa0886f60272df6d72a4fb4b6bba5cd123ce7f9
victorfriedrich <vf@victorfriedrich.com>
2022-07-13 12:58:55 +0200

    Changes 4

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,8 +11,8 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
+        } else if (query.contains("what")) {
+           return "victor";
         } else { // TODO extend the programm here
             // Changes 3
             return "";
